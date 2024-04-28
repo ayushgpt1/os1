4432d0717ba9fdd026a1c238ad17dfc5faed1490
ayushgpt1 <144159606+ayushgpt1@users.noreply.github.com>
2024-04-28 22:19:52 +0530

    Update 10processes.C

--- 10processes.C
+++ 10processes.C
@@ -1,7 +1,7 @@
 
 # Display top 10 processes by CPU usage
 echo "Top 10 processes by CPU usage:"
-ps -eo pid,%cpu,%mem,cmd --sort=-%cpu | head -n 11#!/bin/bash
+ps -eo pid,%cpu,%mem,cmd --sort=-%cpu | head -n 11
 
 # Display processes with highest memory usage
 echo "Processes with highest memory usage:"
