af184f0440bf6ddb9393dc7c30b64c29eab73287
ayushgpt1 <144159606+ayushgpt1@users.noreply.github.com>
2024-04-28 12:56:17 +0530

    Update releasenumber.c

--- releasenumber.c
+++ releasenumber.c
@@ -1,6 +1,10 @@
-os_version=$(cat /etc/os-release | grep PRETTY_NAME | cut -d"=" -f2 | tr -d '"') 
-echo "operating system version: $os_version"
-os_release=$(cat /etc/os-release | grep VERSION_ID | cut -d"=" -f2 | tr -d '"') 
-echo "release number: $os_release" 
+
+# Display OS version
+os_version=$(cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f2 | tr -d '"')
+echo "Operating System Version: $os_version"
+# Display OS release number
+os_release=$(cat /etc/os-release | grep VERSION_ID | cut -d'=' -f2 | tr -d '"')
+echo "Release Number: $os_release"
+# Display kernel version
 kernel_version=$(uname -r)
-echo "release number: $kernel_version" 
+echo "Kernel Version: $kernel_version"
